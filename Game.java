c26f93a0046d8a648b7bdb660443e26164cf7649
Drayble <106639621+Drayble@users.noreply.github.com>
2022-05-31 22:15:11 -0400

    King of Java V 0.8
    Uploaded base files

---
+++ Game.java
@@ -0,0 +1,34 @@
+package KingOfJava;
+
+public class Game {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 200; i++) {
+            if (i % 50 == 0) {
+                System.out.print("!");
+            }
+            else if (i % 10 == 0) {
+                System.out.print("+");
+            }
+            else {
+                System.out.print("`");
+            }
+        }
+        System.out.println();
+        System.out.println("                                                                                                                                                                                                        ");
+        System.out.println("                                                                                                                                                                                                        ");
+        System.out.println("                                                                                                                                                                                                        ");
+        System.out.println("                                                                                                                                                                                                        ");
+        System.out.println("                                                                                                                                                                                                        ");
+        System.out.println("                                                                                                                                                                                                        ");
+        System.out.println("                                                                                                                                                                                                        ");
+        System.out.println("                                                                                                                                                                                                        ");
+        System.out.println("                                                                                                                                                                                                        ");
+        System.out.println("                                                                                                                                                                                                        ");
+        System.out.println("                                                                                                                                                                                                        ");
+        System.out.println("                                                                                                                                                                                                        ");
+        System.out.println("                                                                                                                                                                                                        ");
+        System.out.println("                                                                                                                                                                                                        ");
+        System.out.println("                                                                                                                                                                                                        ");
+        System.out.println("                                                                                                                                                                                                        ");
+    }
+}
